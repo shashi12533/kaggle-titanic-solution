de1e2ff6f8f0ebfd7f62a9e1bb48d356cd7760d1
shashi bhushan <comingtoiit@gmail.com>
2015-12-20 18:28:04 +0530

    Update Tutorial3.R

--- titanic-master/Tutorial3.R
+++ titanic-master/Tutorial3.R
@@ -1,6 +1,6 @@
-# Trevor Stephens - 10 Jan 2014
+
 # Titanic: Getting Started With R - Part 3: Decision Trees
-# Full guide available at http://trevorstephens.com/
+
 
 # Set working directory and import datafiles
 setwd("~/Kaggle/Titanic")
